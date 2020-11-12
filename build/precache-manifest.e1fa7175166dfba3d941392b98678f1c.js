e327fd12631bd8396a647e2bd773dad88edf57a9
ryandbigelow <67275675+ryandbigelow@users.noreply.github.com>
2020-11-12 10:43:22 -0500

    added build

---
+++ build/precache-manifest.e1fa7175166dfba3d941392b98678f1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0e560fbafe9564986363769200abbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db21dea4b8aab139283",
+    "url": "/static/js/2.3e2286c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3e2286c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03318999c3645f993b86",
+    "url": "/static/js/main.624ea865.chunk.js"
+  },
+  {
+    "revision": "64900b6ee1b2f511d7fe",
+    "url": "/static/js/runtime-main.86bb0f70.js"
+  }
+]);

802bb3b90c8b9d0e1de71b2480af4897e409145d
cdaub <cdaub@om1.com>
2021-01-12 13:46:28 -0500

    Update FavoritesPage.tsx

--- src/components/pages/favorites/FavoritesPage.tsx
+++ src/components/pages/favorites/FavoritesPage.tsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import useAxios from 'axios-hooks'
 import axios from 'axios'
-import { Interface } from 'readline'
-
+import styled from '@emotion/styled'
 // Data returned from Cat API
-
+const FavoritesContainer = styled.div`
+  maxHeight: '25%',
+  maxWidth: '50%',
+  display: 'block',
+  margin: 'auto'
+`
 interface CatData {
   breeds: []
   height: number
@@ -40,111 +44,109 @@ const FavoritesPage: React.FunctionComponent = () => {
 const CatView: React.FunctionComponent = () => {
   type views = 'Favorite' | 'Random'
   const [currentView, setCurrentView] = useState<views>('Favorite')
-  if (currentView === 'Favorite') {
-    return (
-      <div>
-        <h1 style={{ textAlign: 'center' }}>Favorite Cat</h1>
-        <button type="button" onClick={() => setCurrentView('Random')}>
-          Go to Cat Seletion
-        </button>
-        <FavoriteCatImages />
-      </div>
-    )
-  }
-  if (currentView === 'Random') {
-    return (
-      <div>
-        <h1 style={{ textAlign: 'center' }}>Pick your Favorite Cat</h1>
-        <button type="button" onClick={() => setCurrentView('Favorite')}>
-          Browse Favorites
-        </button>
-        <RandomCatImage />
-      </div>
-    )
-  }
-  return <div />
+  return (
+    <>
+      {currentView === 'Favorite' && (
+        <div>
+          <h1 style={{ textAlign: 'center' }}>Favorite Cat</h1>
+          <button type="button" onClick={() => setCurrentView('Random')}>
+            Go to Cat Seletion
+          </button>
+          <FavoriteCatImages />
+        </div>
+      )}
+      {currentView === 'Random' && (
+        <div>
+          <h1 style={{ textAlign: 'center' }}>Pick your Favorite Cat</h1>
+          <button type="button" onClick={() => setCurrentView('Favorite')}>
+            Browse Favorites
+          </button>
+          <RandomCatImage />
+        </div>
+      )}
+    </>
+  )
 }
+
 const FavoriteCatImages: React.FunctionComponent = () => {
-  const [
-    { data: favorites, loading: loadingFavorites, error: errorFavorites },
-    getData
-  ] = useAxios({
-    url: 'https://api.thecatapi.com/v1/favourites',
-    method: 'GET',
-    headers: {
-      'x-api-key': '2606fdf1-086c-48db-a667-a4c1047d3433'
+  const [{ data: favorites, loading: loadingFavorites }] = useAxios(
+    {
+      url: 'https://api.thecatapi.com/v1/favourites',
+      method: 'GET',
+      headers: {
+        'x-api-key': '2606fdf1-086c-48db-a667-a4c1047d3433'
+      },
+      params: {
+        sub_id: 'test-user-1'
+      }
     },
-    params: {
-      sub_id: 'test-user-1'
-    }
-  })
+    { useCache: false }
+  )
+
   const favs = favorites as FavoriteData[]
-  if (favorites) {
-    return (
-      <div
-        style={{
-          maxHeight: '25%',
-          maxWidth: '50%',
-          display: 'block',
-          margin: 'auto'
-        }}
-      >
-        {favs.map((fav: FavoriteData) => {
-          return (
-            <img
-              key={fav.image.id}
-              src={fav.image.url}
-              alt={fav.image.id}
-              style={{ maxWidth: '100%', maxHeight: '100%' }}
-            />
-          )
-        })}
-      </div>
-    )
-  }
-  return <div>loading</div>
+  return (
+    <>
+      {loadingFavorites && <div>loading</div>}
+      {favorites && (
+        <FavoritesContainer>
+          {favs.map((fav: FavoriteData) => {
+            return (
+              <img
+                key={fav.image.id}
+                src={fav.image.url}
+                alt={fav.image.id}
+                style={{ maxWidth: '100%', maxHeight: '100%' }}
+              />
+            )
+          })}
+        </FavoritesContainer>
+      )}
+    </>
+  )
 }
 
 const RandomCatImage: React.FunctionComponent = () => {
-  const [{ data, loading, error }, getData] = useAxios<CatData[]>({
-    url: 'https://api.thecatapi.com/v1/images/search',
-    method: 'GET',
-    headers: {
-      'x-api-key': '2606fdf1-086c-48db-a667-a4c1047d3433'
-    }
-  })
-
-  if (data) {
-    return (
-      <div
-        style={{
-          display: 'block',
-          margin: 'auto'
-        }}
-      >
-        <img
-          src={data[0].url}
-          alt={data[0].id}
-          style={{ maxHeight: 200, margin: 'auto' }}
-        />
-        <div>
-          <button type="button" onClick={() => getData()}>
-            See Next Cat
-          </button>
-          <AddToFavoritesButton {...{ id: data[0].id }} />
+  const [{ data, loading }, getData] = useAxios<CatData[]>(
+    {
+      url: 'https://api.thecatapi.com/v1/images/search',
+      method: 'GET',
+      headers: {
+        'x-api-key': '2606fdf1-086c-48db-a667-a4c1047d3433'
+      }
+    },
+    { useCache: false }
+  )
+  return (
+    <>
+      {data && (
+        <div
+          style={{
+            display: 'block',
+            margin: 'auto'
+          }}
+        >
+          <img
+            src={data[0].url}
+            alt={data[0].id}
+            style={{ maxHeight: 200, margin: 'auto' }}
+          />
+          <div>
+            <button type="button" onClick={() => getData()}>
+              See Next Cat
+            </button>
+            <AddToFavoritesButton {...{ id: data[0].id }} />
+          </div>
         </div>
-      </div>
-    )
-  }
-  if (loading) {
-    return <div> loading </div>
-  }
-  return <div>Error</div>
+      )}
+      {loading && <div> loading </div>}
+    </>
+  )
 }
 
 interface ButtonProps {
   id: string
 }
+
 const AddToFavoritesButton: React.FunctionComponent<ButtonProps> = (
   props: ButtonProps
 ) => {
@@ -164,6 +166,7 @@ const AddToFavoritesButton: React.FunctionComponent<ButtonProps> = (
       if (res.status) setAddedToFavorites(true)
     })
   }
+
   const text = addedToFavorites ? 'Added to Favorites' : 'Add to Favorites'
   return (
     <button
